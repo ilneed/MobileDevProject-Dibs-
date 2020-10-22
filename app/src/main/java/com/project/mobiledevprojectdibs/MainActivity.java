f2c80452a2e87f1e08cdd14636977ff5533e1c37
Peter Paul <ptrplmlln@gmail.com>
2020-10-22 12:07:45 +0800

    Log in + Register update

--- app/src/main/java/com/project/mobiledevprojectdibs/MainActivity.java
+++ app/src/main/java/com/project/mobiledevprojectdibs/MainActivity.java
@@ -2,13 +2,51 @@ package com.project.mobiledevprojectdibs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    static int SPLASH_SCREEN = 3000;
+    //1000 = 1 secprivate
+    //variables
+    Animation topAnim;
+    Animation bottomAnim;
+    ImageView appLogo;
+    TextView appTagline;
+    TextView appName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //hooks
+        appLogo = findViewById(R.id.applogo);
+        appName = findViewById(R.id.appname);
+        appTagline = findViewById(R.id.apptagline);
+
+        appLogo.setAnimation(topAnim);
+        appName.setAnimation(bottomAnim);
+        appTagline.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
     }
 }
