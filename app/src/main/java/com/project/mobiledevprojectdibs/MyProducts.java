5041ee67bc61ea14f1beff830bb5114f8d6e04cf
Peter Paul <ptrplmlln@gmail.com>
2020-11-05 15:12:04 +0800

    Bottom Navigation + Side Navigation + Fragments

--- app/src/main/java/com/project/mobiledevprojectdibs/MyProducts.java
+++ app/src/main/java/com/project/mobiledevprojectdibs/MyProducts.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link notifFragment#newInstance} factory method to
+ * Use the {@link MyProducts#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class notifFragment extends Fragment {
+public class MyProducts extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class notifFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public notifFragment() {
+    public MyProducts() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class notifFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment notifFragment.
+     * @return A new instance of fragment MyProducts.
      */
     // TODO: Rename and change types and number of parameters
-    public static notifFragment newInstance(String param1, String param2) {
-        notifFragment fragment = new notifFragment();
+    public static MyProducts newInstance(String param1, String param2) {
+        MyProducts fragment = new MyProducts();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class notifFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notif, container, false);
+        return inflater.inflate(R.layout.fragment_my_products, container, false);
     }
 }
